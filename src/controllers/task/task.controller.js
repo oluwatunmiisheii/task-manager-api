56906d1166a277782bfbeed7919b1107d98f37a0
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-11-06 02:17:30 +0100

    feat: ad pagination and timestamps

--- src/controllers/task/task.controller.js
+++ src/controllers/task/task.controller.js
@@ -1,160 +1,213 @@
-const Task = require("../../models/task.model")
-const mongoose = require('mongoose');
-const { response } = require("express");
+const Task = require("../../models/task.model");
+const mongoose = require("mongoose");
 
 class Tasks {
-  async createTask (req, res) {
+  async createTask(req, res) {
     try {
       const task = new Task({
         ...req.body,
-        owner: req.user._id
-      })
+        owner: req.user._id,
+      });
       await task.save();
-      
+
       res.status(201).send({
-        message: 'Task created successfully',
+        message: "Task created successfully",
         success: true,
-        data: task
-      })
-    }catch (error) {
+        data: task,
+      });
+    } catch (error) {
       res.status(400).send({
-        message: 'Task not created',
+        message: "Task not created",
         success: false,
-        error
-      })
+        error,
+      });
     }
   }
 
-  async getTasks (req, res) {
+  async getTasks(req, res) {
+    const match = {};
+    const sort = { createdAt: -1 };
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
+
+    const skip = (page - 1) * limit;
+
+    if (req.query.completed) {
+      match.completed = req.query.completed === "true";
+    }
+
+    if (req.query.sortBy) {
+      sort.createdAt = req.query.sortBy === "desc" ? -1 : 1;
+    }
+
     try {
       const ownerId = mongoose.Types.ObjectId(req.user._id);
-      const tasks = await Task.find({ owner: { $in: [ownerId] } }).populate('owner', ['name', 'email', 'age'])
+      // const tasks = await Task.find({ owner: { $in: [ownerId] } }).populate('owner', ['name', 'email', 'age'])
+      const tasks = await Task.aggregate([
+        {
+          $match: {
+            ...match,
+            owner: ownerId,
+          },
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "owner",
+            foreignField: "_id",
+            pipeline: [
+              {
+                $project: {
+                  name: 1,
+                  email: 1,
+                  age: 1,
+                },
+              },
+            ],
+            as: "owner",
+          },
+        },
+        { $unwind: "$owner" },
+        { $sort: sort },
+        {
+          $facet: {
+            meta: [{ $count: "total" }, { $addFields: { page } }],
+            data: [{ $skip: skip }, { $limit: limit }],
+          },
+        }
+        // {
+        //   $project: {
+        //     description: 1,
+        //     completed: 1,
+        //     owner: {
+        //       name: 1,
+        //       age: 1,
+        //     },
+        //   },
+        // },
+      ]);
 
-      if(!tasks) {
+      if (!tasks) {
         return res.status(200).send({
-          message: 'Tasks not found',
+          message: "Tasks not found",
           data: [],
-          success: false
-        })
+          success: false,
+        });
       }
 
       res.status(200).send({
-        message: 'Tasks fetched successfully',
+        message: "Tasks fetched successfully",
         success: true,
-        data: tasks
-      })
-      
+        data: tasks,
+      });
     } catch (error) {
-      console.log("🚀 ~ file: task.controller.js ~ line 45 ~ Tasks ~ getTasks ~ error", error)
       res.status(500).send({
-        message: 'Tasks not fetched',
+        message: "Tasks not fetched",
         success: false,
-        error
-      })
+        error,
+      });
     }
   }
 
-  async getTask (req, res) {
-    const _id = req.params.id
+  async getTask(req, res) {
+    const _id = req.params.id;
     try {
-      const task = await Task.findOne({ _id, owner: req.user._id })
+      const task = await Task.findOne({ _id, owner: req.user._id });
 
-      if(!task) {
+      if (!task) {
         return res.status(404).send({
-          message: 'Task not found',
+          message: "Task not found",
           error: true,
-          success: false
-        })
+          success: false,
+        });
       }
 
       res.status(200).send({
-        message: 'Task fetched successfully',
+        message: "Task fetched successfully",
         success: true,
-        data: task
-      })
+        data: task,
+      });
     } catch (error) {
       res.status(500).send({
-        message: 'Task not fetched',
+        message: "Task not fetched",
         success: false,
-        error
-      })
+        error,
+      });
     }
   }
 
-  async updateTask (req, res) {
-    const _id = req.params.id
-    if(!_id) {
+  async updateTask(req, res) {
+    const _id = req.params.id;
+    if (!_id) {
       return res.status(404).send({
-        error: 'Invalid updates!', 
-        success: false 
-      })
+        error: "Invalid updates!",
+        success: false,
+      });
     }
-    
-    const updates = Object.keys(req.body)
-    const allowedUpdateArray = ['description', 'completed']
-    const isValidOperation = updates.every(update => allowedUpdateArray.includes(update))
 
-    if(!isValidOperation) {
+    const updates = Object.keys(req.body);
+    const allowedUpdateArray = ["description", "completed"];
+    const isValidOperation = updates.every((update) => allowedUpdateArray.includes(update));
+
+    if (!isValidOperation) {
       return res.status(400).send({
-        error: 'Invalid updates!', 
-        success: false 
-      })
+        error: "Invalid updates!",
+        success: false,
+      });
     }
 
     try {
       const task = await Task.findOneAndUpdate({ _id, owner: req.user._id }, req.body, {
         new: true,
-        runValidators: true
-      })
+        runValidators: true,
+      });
 
-      if(!task) {
+      if (!task) {
         return res.status(404).send({
-          message: 'Task not found',
-          success: false
-        })
+          message: "Task not found",
+          success: false,
+        });
       }
 
       res.status(200).send({
-        message: 'Task updated successfully',
+        message: "Task updated successfully",
         success: true,
-        data: task
-      })
-
-    }catch(error) {
+        data: task,
+      });
+    } catch (error) {
       res.status(400).send({
-        message: 'Task not updated',
+        message: "Task not updated",
         success: false,
-        error
-      })
+        error,
+      });
     }
   }
 
-  async deleteTask (req, res) {
-    const _id = req.params.id
+  async deleteTask(req, res) {
+    const _id = req.params.id;
     try {
-      const task = await Task.findOneAndDelete({ _id, owner: req.user._id })
+      const task = await Task.findOneAndDelete({ _id, owner: req.user._id });
 
-      if(!task) {
+      if (!task) {
         return res.status(404).send({
-          message: 'Task not found',
-          success: false
-        })
+          message: "Task not found",
+          success: false,
+        });
       }
 
       res.status(200).send({
-        message: 'Task deleted successfully',
+        message: "Task deleted successfully",
         success: true,
-        data: task
-      })
-
-    }catch(error) {
+        data: task,
+      });
+    } catch (error) {
       res.status(500).send({
-        message: 'Task not deleted',
+        message: "Task not deleted",
         success: false,
-        error
-      })
+        error,
+      });
     }
   }
 }
 
-module.exports = new Tasks()
+module.exports = new Tasks();
--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
-
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
   description: {
     type: String,
     trim: true,
@@ -17,6 +16,8 @@ const Task = mongoose.model('Task', {
     required: true,
     ref: 'User'
   }
-})
+}, { timestamps: true })
+
+const Task = mongoose.model('Task', taskSchema)
 
 module.exports = Task
